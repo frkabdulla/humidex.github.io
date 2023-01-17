fd5328e7ac003774611fa99f0ea2319e0e170fd0
faruk abdulla <farukabdulla237@gmail.com>
2023-01-17 18:36:59 +0530

    Commited

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "41f726a0efmsh3c0c5d83e1660edp16b841jsn05e2d1fb5d39",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5e60b176e4da257605a91516078935af"; // enter your key from openweather API
